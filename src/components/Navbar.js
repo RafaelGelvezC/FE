bcb1e0090f804545b2283ed2f97d9efae3a5e534
RafaelGelvezC <rafael.gelvezc@hotmail.com>
2024-05-28 18:58:27 -0500

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css'; // Importa el archivo CSS
 
 function Navbar() {
   const location = useLocation();
@@ -16,41 +17,38 @@ function Navbar() {
       </div>
       <div className="nav-links">
         <Link to="/" onClick={() => handlePageChange('/')}>Inicio</Link>
-        {currentPage === '/' && (
-          <>
-            <Link to="/productos" onClick={() => handlePageChange('/productos')}>Productos</Link>
-            <Link to="/tramites" onClick={() => handlePageChange('/tramites')}>Trámites Fáciles</Link>
-            <Link to="/cultura" onClick={() => handlePageChange('/cultura')}>Cultura Tributaria</Link>
-            <Link to="/login" onClick={() => handlePageChange('/login')}>Iniciar Sesión</Link>
-          </>
-        )}
-        {currentPage === '/productos' && (
-          <>
+        <div className="dropdown">
+          <Link to="/productos" onClick={() => handlePageChange('/productos')}>Productos</Link>
+          <div className="dropdown-content">
             <Link to="/productos/impuestos" onClick={() => handlePageChange('/productos/impuestos')}>Liquidación de Impuestos</Link>
             <Link to="/productos/pagos" onClick={() => handlePageChange('/productos/pagos')}>Liquidación de Pagos</Link>
-          </>
-        )}
-        {currentPage === '/tramites' && (
-          <>
+          </div>
+        </div>
+        <div className="dropdown">
+          <Link to="/tramites" onClick={() => handlePageChange('/tramites')}>Trámites Fáciles</Link>
+          <div className="dropdown-content">
             <Link to="/tramites/impuestos" onClick={() => handlePageChange('/tramites/impuestos')}>Impuestos</Link>
             <Link to="/tramites/pqrs" onClick={() => handlePageChange('/tramites/pqrs')}>Buzón y PQRS</Link>
             <Link to="/tramites/rut" onClick={() => handlePageChange('/tramites/rut')}>RUT Tips</Link>
             <Link to="/tramites/devoluciones" onClick={() => handlePageChange('/tramites/devoluciones')}>Devoluciones y Compensaciones</Link>
             <Link to="/tramites/facilidades" onClick={() => handlePageChange('/tramites/facilidades')}>Facilidades</Link>
             <Link to="/tramites/sucesiones" onClick={() => handlePageChange('/tramites/sucesiones')}>Sucesiones</Link>
-          </>
-        )}
-        {currentPage === '/cultura' && (
-          <>
+          </div>
+        </div>
+        <div className="dropdown">
+          <Link to="/cultura" onClick={() => handlePageChange('/cultura')}>Cultura Tributaria</Link>
+          <div className="dropdown-content">
             <Link to="/cultura/inquietudes" onClick={() => handlePageChange('/cultura/inquietudes')}>Deja tus Inquietudes</Link>
-          </>
-        )}
+          </div>
+        </div>
+        <Link to="/login" onClick={() => handlePageChange('/login')}>Iniciar Sesión</Link>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+
 
 //verificar bien en tramites porque no se ven todas la opciones
 //hacer que el menu se despliegue desde navbar y que no se vea en la pagina
